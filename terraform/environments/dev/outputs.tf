1026aff95d2af6503730ab7e2776b097aefd7e34
Ashish Verma <ashish.verma.82@gmail.com>
2026-01-07 16:36:16 +1100

    fix: sync outputs.tf with Phase 1 documentation

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,10 +1,14 @@
 output "documents_bucket_name" {
-  description = "Name of the created documents bucket"
+  description = "Name of the documents S3 bucket"
   value       = module.documents_bucket.bucket_name
 }
 
 output "embeddings_bucket_name" {
-  description = "Name of the created embeddings bucket"
+  description = "Name of the embeddings S3 bucket"
   value       = module.embeddings_bucket.bucket_name
 }
 
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = module.ecr.repository_url
+}
